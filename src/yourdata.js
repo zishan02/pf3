ef0d6476387f78bb1555d4858e42f30991f961a2
Zishan <zishanmohammed02@gmail.com>
2021-08-12 23:56:12 +0530

    commit

--- src/yourdata.js
+++ src/yourdata.js
@@ -119,7 +119,7 @@ export default {
   aboutParaThree:
     "",
   aboutImage:
-    "",
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
 
   //   End About Section ---------------------
 
