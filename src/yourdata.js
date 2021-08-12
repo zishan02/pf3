4b7998e3fbcf1d5fb09850bb27ae0cf58b19cb55
Zishan <zishanmohammed02@gmail.com>
2021-08-12 23:52:16 +0530

    about me updated

--- src/yourdata.js
+++ src/yourdata.js
@@ -113,7 +113,7 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "I am a M.Tech Graduate and is currently working with Oracle India. I have a total of 5 Years of Experience in various technologies",
   aboutParaTwo:
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   aboutParaThree:
