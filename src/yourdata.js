751ce24bfdee87b05c7a5dbff806e05f6fc96c3f
Zishan <zishanmohammed02@gmail.com>
2021-08-15 23:31:09 +0530

    d

--- src/yourdata.js
+++ src/yourdata.js
@@ -51,7 +51,16 @@ export default {
       //Project URL - Add Your Project Url Here
       url: "http://trackcovid-19.co.in/",
     },
-    
+    {
+      title: "Project Two", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://mdzishan.tech/",
+    },
 
 
     /*
