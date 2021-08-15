49c17a33746ca8f2ba5fecb010e7c7da64488c8b
Zishan <zishanmohammed02@gmail.com>
2021-08-15 23:15:46 +0530

    fix bug

--- src/yourdata.js
+++ src/yourdata.js
@@ -49,7 +49,7 @@ export default {
         "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
       url: "http://trackcovid-19.co.in/",
-    },
+    }
 
     /*
     If You Want To Add More Project just Copy and Paste This At The End
